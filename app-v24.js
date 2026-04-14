b97979d6cecae3170a76559c06e306d6950967f0
abdoolelamin-commits <abdoolelamin@gmail.com>
2026-04-15 00:07:04 +0300

    Create app-v24.js

---
+++ app-v24.js
@@ -0,0 +1,5 @@
+const app = document.getElementById("app");
+app.innerHTML = `
+  <div style="padding:10px;color:green;font-weight:bold;">APP V24 LOADED</div>
+  <div>لو ظهرت هذه الرسالة، فملف الجافاسكربت صار يعمل.</div>
+`;
